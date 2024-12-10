442ce4e2eac5dd129273a173f40a6d2ef806765d
Prabhkirat Singh <prabhkirat.singh@thewitslab.com>
2024-12-10 18:03:21 +0530

    select and on_select added for seller

--- apps/backend/src/controllers/onest/bap/onSelect.ts
+++ apps/backend/src/controllers/onest/bap/onSelect.ts
@@ -20,87 +20,134 @@ const onSelectConsultationController = (
 	next: NextFunction
 ) => {
 	try {
-		// const {
-		// 	context,
-		// 	message: {
-		// 		order: { provider, items, payments, fulfillments },
-		// 	},
-		// } = req.body;
-		console.log("🚀 ~ req.body:", req.body)
-		
-		// const { id, type, stops } = fulfillments[0];
-		// const responseMessage = {
-		// 	order: {
-		// 		provider: {
-		// 			...provider,
-		// 			locations: [{ id: uuidv4() }],
-		// 		},
+		const {
+			context,
+			message: {
+				order: { provider, items, quote },
+			},
+		} = req.body;
 
-		// 		items: items.map(
-		// 			({ location_ids, ...items }: { location_ids: any }) => items
-		// 		),
+		delete provider?.descriptor
 
-		// 		billing: {
-		// 			name: "ONDC buyer",
-		// 			address:
-		// 				"22, Mahatma Gandhi Rd, Craig Park Layout, Ashok Nagar, Bengaluru, Karnataka 560001",
-		// 			state: {
-		// 				name: "Karnataka",
-		// 			},
-		// 			city: {
-		// 				name: "Bengaluru",
-		// 			},
-		// 			tax_id: "XXXXXXXXXXXXXXX",
-		// 			email: "nobody@nomail.com",
-		// 			phone: "9886098860",
-		// 		},
+		const npFeesTag = quote?.breakup
+			.filter((item: any) => item.title === "NP Fees")
+			.map((npTag: any) => {
+				const tags = npTag.tags;
+				return tags.find((tag: any) => tag.descriptor.code === "NP_FEES");
+			});
 
-		// 		fulfillments: [
-		// 			{
-		// 				id,
-		// 				type,
-		// 				stops: [
-		// 					{
-		// 						id: stops[0].id,
-		// 						location: {
-		// 							gps: "12.974002,77.613458",
-		// 							address: "My House #, My buildin",
-		// 							city: {
-		// 								name: "Bengaluru",
-		// 							},
-		// 							country: {
-		// 								code: "IND",
-		// 							},
-		// 							area_code: "560001",
-		// 							state: {
-		// 								name: "Karnataka",
-		// 							},
-		// 						},
-		// 						contact: {
-		// 							phone: "9886098860",
-		// 						},
-		// 						time: stops[0].time,
-		// 					},
-		// 				],
-		// 			},
-		// 		],
-				
-		// 		payments,
-		// 	},
-		// };
+		const responseMessage = {
+			order: {
+				provider: {
+					...provider,
+				},
+
+				items: items,
+				fulfillments: [
+					{
+						id: "1",
+						customer: {
+							person: {
+								name: "Sanjay",
+								gender: "Male",
+								age: "35",
+								skills: [
+									{
+										code: "Android",
+										name: "Android",
+									},
+									{
+										code: "AWS",
+										name: "AWS",
+									},
+								],
+								languages: [
+									{
+										code: "en",
+										name: "english",
+									},
+									{
+										code: "ml",
+										name: "Malayalam",
+									},
+									{
+										code: "hi",
+										name: "Hindi",
+									},
+								],
+								tags: [
+									{
+										descriptor: {
+											code: "EMP_DETAILS",
+											name: "Employee Details",
+										},
+										list: [
+											{
+												descriptor: {
+													code: "EXPECTED_SALARY",
+												},
+												value: "1000000",
+											},
+											{
+												descriptor: {
+													code: "TOTAL_EXPERIENCE",
+													name: "Total Experience",
+												},
+												value: "P4Y",
+											},
+										],
+									},
+									{
+										descriptor: {
+											code: "documents",
+											name: "Documents",
+										},
+										list: [
+											{
+												descriptor: {
+													code: "DOC_TYPE",
+												},
+												value: "resume",
+											},
+											{
+												descriptor: {
+													code: "link",
+												},
+												value: "https://example.com/resume.pdf",
+											},
+											{
+												descriptor: {
+													code: "FILE_FORMAT",
+												},
+												value: "pdf",
+											},
+										],
+									},
+									...npFeesTag
+								],
+							},
+						},
+						contact: {
+							phone: "9999999999",
+							email: "abc@abc.bc",
+						},
+					},
+				],
+				quote: quote,
+			},
+		};
 		return responseBuilder(
 			res,
 			next,
-			{},
-			{},
+			context,
+			responseMessage,
 			`${req.body.context.bpp_uri}${
 				req.body.context.bpp_uri.endsWith("/") ? "init" : "/init"
 			}`,
 			`init`,
-			"services"
+			"onest"
 		);
 	} catch (error) {
 		next(error);
 	}
 };
-
